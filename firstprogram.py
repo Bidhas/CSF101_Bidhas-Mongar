f7c6eb26298ac55cc13998345c3f23338343d2e3
Bidhas <tepidtunda@gmail.com>
2025-07-29 14:37:27 +0600

    Basic intro codes

--- firstprogram.py
+++ firstprogram.py
@@ -1,2 +1,8 @@
-print("Hello SWE!")
-print("whats up brother ")
+"""print("Hello SWE!")
+a = input("Enter Your Desired No. :")
+b = input("Enter Your Second Desired No. :")
+sum = a + b
+print("The Sum of a and b is", sum)"""
+name = input ("Enter Your Name: ")
+age = input("Enter Youe Age: ")
+print("Sohkha! You are", name, "and you are", age, "years old, huh?")
