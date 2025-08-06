15d714e7d2b6490c3bcb4817a0a30fb0587c982f
Bidhas <tepidtunda@gmail.com>
2025-08-06 15:04:05 +0600

    directory changes and arithmetic exercises

---
+++ Operators/arithmetic.py
@@ -0,0 +1,8 @@
+#Exercise 1 : Arithmetic Operators
+a , b = 15 , 4 
+print(f"a={a} , b= {b}")
+print() 
+print(f"Addition ={a + b}")
+print(f"Substraction ={a - b}")
+print(f"Multiplication ={a * b}")
+print(f"Division ={a / b}")
